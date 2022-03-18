55cd5dec6b8d25367c01b6b04fc8b5b64870b850
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2022-03-19 03:12:35 +0530

    Created product view

--- scripts/buyMerch.js
+++ scripts/buyMerch.js
@@ -37,7 +37,6 @@ function displayCategory(categoryClass) {
 
 //add eventlisteners to all category-buttons
 categoryButtons.forEach(function (button) {
-  console.log(button);
   if (button.id !== "all") {
     button.addEventListener("click", function () {
       displayCategory(button.id);
@@ -47,4 +46,65 @@ categoryButtons.forEach(function (button) {
       displayAllItems();
     });
   }
+});
+
+//get left and right buttons
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+//get preview image
+let previewImage = document.getElementById("preview-image");
+
+//function for switch images
+function switchImage(direction) {
+  let currentImage = previewImage.src;
+  let currentImageIndex = parseInt(currentImage.slice(-5, -4));
+  if (direction === "left") {
+    if (currentImageIndex != 1) {
+      previewImage.src =
+        currentImage.slice(0, -5) + (currentImageIndex - 1) + ".jpg";
+      console.log(previewImage.src);
+    }
+  } else if (direction === "right") {
+    if (currentImageIndex != 4) {
+      previewImage.src =
+        currentImage.slice(0, -5) + (currentImageIndex + 1) + ".jpg";
+      console.log(previewImage.src);
+    }
+  }
+}
+
+//add eventlisteners to arrows for switching images
+leftArrow.addEventListener("click", function () {
+  switchImage("left");
+});
+rightArrow.addEventListener("click", function () {
+  switchImage("right");
+});
+
+//get all add-to-cart buttons
+const addToCartButtons = document.querySelectorAll(".item-card .add-to-cart");
+console.log(addToCartButtons);
+
+function viewProduct() {
+  document.body.className = "body-blur";
+  document.getElementById("product-view").className = "product-view";
+  window.scrollTo(0, 0);
+}
+
+//add eventlisteners to all add-to-cart buttons in item view
+addToCartButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    viewProduct();
+  });
+});
+
+function hideProduct() {
+  document.body.className = "";
+  document.getElementById("product-view").className = "hidden";
+}
+
+document.getElementById("close-button").addEventListener("click", function () {
+  document.body.className = "";
+  document.getElementById("product-view").className = "hidden";
 });
